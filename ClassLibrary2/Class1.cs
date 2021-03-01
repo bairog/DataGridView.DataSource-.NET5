ad5547885b23a711dcc2f74adb3ece410841b5ce
bairog <44995181+bairog@users.noreply.github.com>
2021-03-01 11:28:02 +0300

    Add files via upload

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("WindowsFormsApp2")]
+namespace ClassLibrary2
+{
+    internal class Address
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+    }
+}
